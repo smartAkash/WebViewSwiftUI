58d7f83debd4b83e66d9891b5ae6cf865c58cc38
Akash <akash.nara@smartsensesolutions.com>
2021-10-18 11:10:53 +0530

    Initial Commit

---
+++ WebViewSwiftUI/WebViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewSwiftUIApp.swift
+//  WebViewSwiftUI
+//
+//  Created by Akash on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
